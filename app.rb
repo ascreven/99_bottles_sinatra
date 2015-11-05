e8d1350cedd0c768ab3cb10cb5c049a0bb35a6ba
Alexandra <akscreven@gmail.com>
2015-11-04 19:43:27 -0500

    functions are working

--- app.rb
+++ app.rb
@@ -1,2 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
+names = ["andre", "nick", "alex", "andy"]
+
+get '/' do
+ erb :index
+end
+
+get '/names' do
+  @names = names
+  erb :names
+end
+
+get '/:num_bottles' do
+  @num_bottles = params[:num_bottles].to_i
+  @next = @num_bottles - 1
+  erb :num_bottles
+end
